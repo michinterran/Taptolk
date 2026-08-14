c65402376d4560de554bac6398ea54153e147e64
michinterran <michinterran@gmail.com>
2026-08-14 12:56:16 +0900

    fix: accept QR bundle identifiers

--- packages/application/src/qr-svg-bundle-service.ts
+++ packages/application/src/qr-svg-bundle-service.ts
@@ -1,7 +1,7 @@
 import { type AdminAuthorizationContext, authorizeAdminAction } from "@taptolk/domain";
 import { assertAdminAuthorized } from "./authorization-error.js";
 
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{12}$/iu;
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu;
 
 export interface QrSvgBundleActor {
   authorization: AdminAuthorizationContext;
